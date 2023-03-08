d2a6726e4a90245fb45144e08e5cd0ccf2ee94f6
Eyob Tadele-Manhardt <eyobtade@buffalo.edu>
2023-03-08 11:28:47 -0500

    Update dbt yaml files.

---
+++ dbt/models/divvy_core/dim_neighborhoods.sql
@@ -0,0 +1,5 @@
+{{ config(materialized='table') }}
+
+
+select *
+from {{ ref('divvy_stations_lookup') }}
--- dbt/models/divvy_core/facts_divy_trips.sql
+++ dbt/models/divvy_core/facts_divy_trips.sql
@@ -1,23 +1,44 @@
 {{ config(materialized='table') }}
 
-with tripdata as (
-    select *, TIMESTAMP_DIFF(ended_at, started_at, MINUTE) AS duration_minutes
-    from {{ ref('stg_divvy_tripdata') }}
+WITH tripdata AS (
+    SELECT 
+        ride_id, 
+        rideable_type,
+        membership_status,
+        start_station_name,
+        end_station_name,
+        started_at,
+        ended_at,
+        TIMESTAMP_DIFF(ended_at, started_at, MINUTE) AS duration_minutes
+    FROM {{ ref('stg_divvy_tripdata') }}
+),
+neighborhoods AS (
+    SELECT 
+        station_id,
+        station_name,
+        CONCAT(CAST(lat AS STRING), ',', CAST(lng AS STRING)) AS location,
+        primary_neighborhood AS neighborhood
+    FROM {{ ref('dim_neighborhoods') }}
+
 )
-SELECT
-    ride_id,
-    start_station_id,
-    end_station_id,
-    CASE WHEN membership_status = 'member' THEN 'Annual' ELSE 'casual' END AS membership_status,
+SELECT 
+    ride_id, 
     rideable_type,
+    membership_status,
+    start_station_info.station_id AS start_station_id,
     start_station_name,
     end_station_name,
+    end_station_info.station_id AS end_station_id,
     started_at,
     ended_at,
     duration_minutes,
-    CONCAT(CAST(start_lat AS STRING), ',', CAST(start_lng AS STRING)) AS start_location,
-    CONCAT(CAST(end_lat AS STRING), ',', CAST(end_lng AS STRING)) AS end_location
-FROM tripdata
--- total miniutes in a day: 24 * 60 = 1440
--- if duration >= 24 * 60 => invalid trip
+    start_station_info.neighborhood AS start_neighborhood,
+    start_station_info.location AS start_location,
+    end_station_info.neighborhood AS end_neighborhood,
+    end_station_info.location AS end_location
+FROM tripdata 
+INNER JOIN  neighborhoods AS start_station_info
+ON tripdata.start_station_name = start_station_info.station_name
+INNER JOIN neighborhoods AS end_station_info
+ON tripdata.end_station_name = end_station_info.station_name
 WHERE duration_minutes BETWEEN  1 AND 24 * 60 - 1
